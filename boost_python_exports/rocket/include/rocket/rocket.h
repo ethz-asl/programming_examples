5fbb17d95abac5eb7bf2f92ccc2950c5955c4d2e
Paul Furgale <paul.furgale@gmail.com>
2014-08-24 22:30:07 +0200

    Rocket

---
+++ boost_python_exports/rocket/include/rocket/rocket.h
@@ -0,0 +1,47 @@
+#ifndef _ROCKET_H_
+#define _ROCKET_H_
+
+#include <Eigen/Core>
+
+namespace rocket {
+
+class Rocket {
+ public:
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+  Rocket();
+  Rocket(double max_speed);
+  Rocket(double max_speed, const Eigen::Matrix2d& inertia_matrix);
+  virtual ~Rocket();
+
+  double getMaxSpeed() const;
+  void setMaxSpeed(double max_speed);
+
+  void setInertiaMatrix(const Eigen::Matrix2d& inertia_matrix);
+  Eigen::Matrix2d& getInertiaMatrix();
+  const Eigen::Matrix2d& getInertiaMatrix() const;
+ private:
+  double max_speed_;
+  Eigen::Matrix2d inertia_matrix_;
+};
+
+class Booster : public Rocket {
+ public:
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+  Booster();
+  Booster(double max_speed, 
+          const Eigen::Matrix2d& inertia_matrix,
+          unsigned num_boosts);
+  virtual ~Booster();
+
+  unsigned getNumBoosts() const;
+  void setNumBoosts(unsigned nb);
+  
+ private:
+  unsigned num_boosts_;
+};
+
+} // namespace rocket
+
+
+#endif /* _ROCKET_H_ */
---
+++ boost_python_exports/rocket/src/rocket.cpp
@@ -0,0 +1,71 @@
+#include <rocket/rocket.h>
+
+namespace rocket {
+Rocket::Rocket() : 
+max_speed_(100.0), inertia_matrix_(Eigen::Matrix2d::Identity()) {
+
+}
+
+Rocket::Rocket(double max_speed) : 
+max_speed_(max_speed), inertia_matrix_(Eigen::Matrix2d::Identity()) {
+
+}
+
+Rocket::Rocket(double max_speed, const Eigen::Matrix2d& inertia_matrix) : 
+max_speed_(max_speed), inertia_matrix_(inertia_matrix) {
+
+}
+
+Rocket::~Rocket() {
+
+}
+
+
+double Rocket::getMaxSpeed() const {
+  return max_speed_;
+}
+
+void Rocket::setMaxSpeed(double max_speed) {
+  max_speed_ = max_speed;
+}
+
+
+void Rocket::setInertiaMatrix(const Eigen::Matrix2d& inertia_matrix) {
+  inertia_matrix_ = inertia_matrix;
+}
+
+Eigen::Matrix2d& Rocket::getInertiaMatrix() {
+  return inertia_matrix_;
+}
+
+const Eigen::Matrix2d& Rocket::getInertiaMatrix() const {
+  return inertia_matrix_;
+}
+
+
+Booster::Booster() {
+
+}
+
+Booster::Booster(double max_speed, 
+                 const Eigen::Matrix2d& inertia_matrix,
+                 unsigned num_boosts) :
+    Rocket(max_speed, inertia_matrix), num_boosts_(num_boosts){
+
+}
+
+Booster::~Booster() {
+
+}
+
+unsigned Booster::getNumBoosts() const {
+  return num_boosts_;
+}
+
+void Booster::setNumBoosts(unsigned nb) {
+  num_boosts_ = nb;
+}
+
+
+
+} // namespace rocket
